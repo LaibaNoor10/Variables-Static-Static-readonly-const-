47b172db020a5d234f1577749ef1c37bd5340022
LaibaNoor10 <125575046+LaibaNoor10@users.noreply.github.com>
2023-05-04 22:25:41 +0530

    Add files via upload

---
+++ Variables/Variables.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    #region Write details of a company by using variables
+    public class Variables
+    {
+        //Declaration of the static, readonly static and const variable
+
+        static readonly string companyName; //Common but not changed frequently
+        static string compAddress;//Globally Common for all the object
+        static string phoneNumber;
+        const int founded = 2007; //Universally common and can't be changed
+
+
+        //Using Constructor to initialize the values
+        static Variables()
+        {
+            companyName = "Invia Pvt. Ltd. Company";
+            compAddress = "Sydney, Australia";
+            phoneNumber = "9870123456";
+        }
+
+        //By this static method we can get the company details
+        public static void CompanyDetails()
+        {
+            Console.WriteLine("Company Name : "+companyName);
+            Console.WriteLine("Address : "+compAddress);
+            Console.WriteLine("Contact No : "+phoneNumber);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Founded : "+founded);
+            CompanyDetails();
+            Console.ReadLine();
+
+        }
+    }
+    #endregion
+}
